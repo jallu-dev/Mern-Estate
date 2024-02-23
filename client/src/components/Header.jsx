df603c556981f652917d6a6010434da9616b4739
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2024-02-23 17:26:26 +0530

    created and run the server

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import {FaSearch} from 'react-icons/fa'
 import {Link} from 'react-router-dom'
-import Home from '../pages/Home'
 
 export default function Header() {
   return (
