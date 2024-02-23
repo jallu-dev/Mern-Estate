5110dc623a76de5f0fa518906d8609648c7b3686
Liyasdeen Mohammed Zahran <zzzahrannnldeen@gmail.com>
2024-02-23 16:59:41 +0530

    Create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import {FaSearch} from 'react-icons/fa'
+import {Link} from 'react-router-dom'
+import Home from '../pages/Home'
+
+export default function Header() {
+  return (
+    <header className='bg-slate-200 shadow-md flex justify-between items-center max-w-3xl mx-auto p-3'>
+        <Link to='/'><h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
+            <span className='text-slate-500'>Sahand</span><span className='text-slate-700'>Estate</span>
+        </h1></Link>
+        
+        <form className='bg-slate-100 px-3 py-1 rounded-lg flex justify-between items-center text-slate-600'>
+            <input type="text" placeholder='Search...' className='bg-transparent outline-none w-24 sm:w-64'/>
+            <FaSearch />
+        </form>
+        <ul className='flex gap-4'>
+            <li className='text-slate-700 hidden sm:inline border
+            hover:border-black cursor-pointer px-2 py-1 rounded-md hover:bg-slate-800 hover:text-slate-100'><Link to='/'>Home</Link></li>
+            <li className='text-slate-700 hidden sm:inline border
+            hover:border-black cursor-pointer px-2 py-1 rounded-md hover:bg-slate-800 hover:text-slate-100'><Link to='/about'>About</Link></li>
+            <li className='text-slate-700 sm:inline border
+            hover:border-black cursor-pointer px-2 py-1 rounded-md hover:bg-slate-800 hover:text-slate-100'><Link to='/sign-in'>Sign In</Link></li>
+        </ul>
+    </header>
+  )
+}
